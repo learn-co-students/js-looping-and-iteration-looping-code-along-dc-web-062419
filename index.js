1076851e79ce257a8643f4a3941b1cab7899ec05
SkaMateria <skamateria@gmail.com>
2019-08-05 20:12:58 -0400

    Done.

--- index.js
+++ index.js
@@ -15,26 +15,26 @@
 function writeCards(stringName, eventName) {
     const thankYouMessages = [];
     for (let i = 0; i < stringName.length; i++) {
-        thankYouMessages.push(`Thank you, ${stringName[i]}, for the wonderful ${eventName} gift.`);
+        thankYouMessages.push(`Thank you, ${stringName[i]}, for the wonderful ${eventName} gift!`);
     }
     return thankYouMessages;
 }
-    // writeCards(stringName, eventName);
+// writeCards(stringName, eventName);
 
 // ----------------------------------------
 // while ([condition]) {
     //[loop body]
-// }
-
-
-
-// ---------------------------------------------
-
-function countdown(posInt) {
-    let i = posInt;
-    while (i >= 0) {
-        console.log(i--);
-    }
-    return posInt;
+    // }
+    
+    
+    
+    // ---------------------------------------------
+    
+    function countdown(posInt) {
+        let i = posInt;
+        while (i >= 0) {
+            console.log(i--);
+        }
+        return posInt;
 }
-    countdown(posInt);
+    countdown();
